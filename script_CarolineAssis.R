286f39a58457541753c41dd059b1c2b4e2b4d8a1
Carol Assis <57148658+cahssis@users.noreply.github.com>
2021-04-25 21:03:00 -0300

    Update script_CarolineAssis.R

--- script_CarolineAssis.R
+++ script_CarolineAssis.R
@@ -17,13 +17,11 @@ pacf(federal.ts)
 stationary.test(federal.ts, method = "pp")
 # dado que não é estacionários veremos quantas diferenças serão necessárias
 ndiffs(federal.ts)
-# também podemos aplicar uma transformação, nesse caso vamos usar a 
-#transformação log
+# também podemos aplicar uma transformação, nesse caso vamos usar a transformação log
 federal.ts_transf = ts(log(federal.ts), start = 1990, end = 2019, 
                        frequency = 1)
 ndiffs(federal.ts_transf)
-# vemos que mesmo com a transformação continua sendo não estácionária, então
-#vamos partir para as diferenciações
+# vemos que mesmo com a transformação continua sendo não estácionária, então vamos partir para as diferenciações
 federal.ts_diff = diff(diff(federal.ts))
 stationary.test(federal.ts_diff, method = "pp")
 ndiffs(federal.ts_diff)
